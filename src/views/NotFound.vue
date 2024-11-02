403d066c2f72da51e3cb91d32f3b272be263a89f
mkbcn <matthiasklees@outlook.de>
2024-11-02 15:12:32 +0100

    modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/router/index.js 	new file:   src/views/NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+
+//------------------  SCRIPT ------------------------
+
+<script setup>
+
+
+</script>
+
+//------------------- TEMPLATE -----------------------
+
+<template>
+
+<h1>Ops! 404 Page not found!</h1>
+<RouterLink to="/"><h2>GO TO HOMEPAGE</h2></RouterLink>
+
+</template>
+
+//------------------- STYLE --------------------------
+
+<style scoped>
+
+
+</style>
